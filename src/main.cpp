694fba5e83b1c6cabca05bba156341c5ecf8be9b
Agus priyono <46214038+Sugapri@users.noreply.github.com>
2024-06-30 07:48:49 +0700

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,212 @@
+#include <Arduino.h>
+
+// UTFT_Demo_320x240 
+// Copyright (C)2015 Rinky-Dink Electronics, Henning Karlsen. All right reserved
+// web: http://www.RinkyDinkElectronics.com/
+//
+// This program is a demo of how to use most of the functions
+// of the library with a supported display modules.
+//
+// This demo was made for modules with a screen resolution 
+// of 320x240 pixels.
+//
+// This program requires the UTFT library.
+//
+
+//################################################
+// GLUE class that implements the UTFT API
+// replace UTFT include and constructor statements
+// remove UTFT font declaration e.g. SmallFont
+//################################################
+
+#include <SPI.h>          // f.k. for Arduino-1.5.2
+#include "Adafruit_GFX.h"// Hardware-specific library
+#include <MCUFRIEND_kbv.h>
+MCUFRIEND_kbv tft;
+
+#define	TFT_BLACK   0x0000
+#define	TFT_BLUE    0x001F
+#define	TFT_RED     0xF800
+#define	TFT_GREEN   0x07E0
+#define TFT_CYAN    0x07FF
+#define TFT_MAGENTA 0xF81F
+#define TFT_YELLOW  0xFFE0
+#define TFT_WHITE   0xFFFF
+#define TFT_GREY    0x5AEB
+
+
+
+float sx = 0, sy = 1, mx = 1, my = 0, hx = -1, hy = 0;    // Saved H, M, S x & y multipliers
+float sdeg=0, mdeg=0, hdeg=0;
+uint16_t osx=120, osy=120, omx=120, omy=120, ohx=120, ohy=120;  // Saved H, M, S x & y coords
+uint16_t x0=0, x1=0, yy0=0, yy1=0;
+uint32_t targetTime = 0;                    // for next 1 second timeout
+
+static uint8_t conv2d(const char* p); // Forward declaration needed for IDE 1.6.x
+uint8_t hh=conv2d(__TIME__), mm=conv2d(__TIME__+3), ss=conv2d(__TIME__+6);  // Get H, M, S from compile time
+
+bool initial = 1;
+
+// Declare which fonts we will be using
+//extern uint8_t SmallFont[];      //GLUE defines as GFXFont ref
+
+// Set the pins to the correct ones for your development shield
+// ------------------------------------------------------------
+// Arduino Uno / 2009:
+// -------------------
+// Standard Arduino Uno/2009 shield            : <display model>,A5,A4,A3,A2
+// DisplayModule Arduino Uno TFT shield        : <display model>,A5,A4,A3,A2
+//
+// Arduino Mega:
+// -------------------
+// Standard Arduino Mega/Due shield            : <display model>,38,39,40,41
+// CTE TFT LCD/SD Shield for Arduino Mega      : <display model>,38,39,40,41
+//
+// Remember to change the model parameter to suit your display module!
+//UTFT myGLCD(ITDB32S,38,39,40,41);
+
+
+
+void setup(void) {
+  Serial.begin(9600);
+    uint32_t when = millis();
+    //    while (!Serial) ;   //hangs a Leonardo until you connect a Serial
+    if (!Serial) delay(5000);           //allow some time for Leonardo
+    Serial.println("Serial took " + String((millis() - when)) + "ms to start");
+    //    tft.reset();                 //hardware reset
+    uint16_t ID = tft.readID(); //
+    Serial.print("ID = 0x");
+    Serial.println(ID, HEX);
+    if (ID == 0xD3D3) ID = 0x9481; // write-only shield
+//    ID = 0x9329;                             // force ID
+    tft.begin(ID);
+  
+  tft.fillScreen(TFT_BLACK);
+  //tft.fillScreen(TFT_RED);
+  //tft.fillScreen(TFT_GREEN);
+  //tft.fillScreen(TFT_BLUE);
+  //tft.fillScreen(TFT_BLACK);
+  //tft.fillScreen(TFT_GREY);
+  
+  tft.setTextColor(TFT_WHITE, TFT_GREY);  // Adding a background colour erases previous text automatically
+  
+  // Draw clock face
+  tft.fillCircle(120, 120, 118, TFT_GREENYELLOW);
+  tft.fillCircle(120, 120, 116, TFT_YELLOW);
+  tft.fillCircle(120, 120, 114, TFT_BLUE);
+  tft.fillCircle(120, 120, 112, TFT_PINK);
+  tft.fillCircle(120, 120, 110, TFT_BLACK);
+
+  // Draw 12 lines
+  for(int i = 0; i<360; i+= 30) {
+    sx = cos((i-90)*0.0174532925);
+    sy = sin((i-90)*0.0174532925);
+    x0 = sx*114+120;
+    yy0 = sy*114+120;
+    x1 = sx*100+120;
+    yy1 = sy*100+120;
+
+    tft.drawLine(x0, yy0, x1, yy1, TFT_GREEN);
+  }
+
+  // Draw 60 dots
+  for(int i = 0; i<360; i+= 6) {
+    sx = cos((i-90)*0.0174532925);
+    sy = sin((i-90)*0.0174532925);
+    x0 = sx*102+120;
+    yy0 = sy*102+120;
+    // Draw minute markers
+    tft.drawPixel(x0, yy0, TFT_WHITE);
+    
+    // Draw main quadrant dots
+    if(i==0 || i==180) tft.fillCircle(x0, yy0, 2, TFT_WHITE);
+    if(i==90 || i==270) tft.fillCircle(x0, yy0, 2, TFT_WHITE);
+  }
+
+  // Menggambar angka 1-12 sebagai jam analog
+  for (int i = 1; i <= 12; ++i) {
+    // Menghitung posisi titik tengah untuk setiap angka
+    int centerX = tft.width() / 2;
+    int centerY = tft.height() / 2;
+    float angle = radians((i - 3) * 30);  // Menghitung sudut untuk masing-masing angka (mulai dari 1 jam)
+    int x = centerX +8 + cos(angle) * 90;  // Jarak dari pusat untuk menggambar angka (disesuaikan dengan radius yang diinginkan)
+    int y = centerY -33 + sin(angle) * 90;
+
+    // Menulis angka ke layar TFT
+    tft.setTextColor(TFT_RED, TFT_BLACK);
+    tft.setTextSize(1);
+    tft.setCursor(x - 10, y - 10);  // Penyesuaian posisi teks agar berada di tengah angka
+    tft.print(i);
+    }
+  ///////////////////////////////////
+
+  tft.fillCircle(120, 121, 3, TFT_WHITE);
+
+  // Draw text at position 120,260 using fonts 4
+  // Only font numbers 2,4,6,7 are valid. Font 6 only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : . - a p m
+  // Font 7 is a 7 segment font and only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : 
+  tft.setTextColor(TFT_RED, TFT_BLACK);
+  tft.setTextSize(3);
+  tft.setCursor(20, 260);
+  tft.println("Time flies");
+
+  targetTime = millis() + 1000; 
+}
+
+void loop() {
+  if (targetTime < millis()) {
+    targetTime += 1000;
+    ss++;              // Advance second
+    if (ss==60) {
+      ss=0;
+      mm++;            // Advance minute
+      if(mm>59) {
+        mm=0;
+        hh++;          // Advance hour
+        if (hh>23) {
+          hh=0;
+        }
+      }
+    }
+
+    // Pre-compute hand degrees, x & y coords for a fast screen update
+    sdeg = ss*6;                  // 0-59 -> 0-354
+    mdeg = mm*6+sdeg*0.01666667;  // 0-59 -> 0-360 - includes seconds
+    hdeg = hh*30+mdeg*0.0833333;  // 0-11 -> 0-360 - includes minutes and seconds
+    hx = cos((hdeg-90)*0.0174532925);    
+    hy = sin((hdeg-90)*0.0174532925);
+    mx = cos((mdeg-90)*0.0174532925);    
+    my = sin((mdeg-90)*0.0174532925);
+    sx = cos((sdeg-90)*0.0174532925);    
+    sy = sin((sdeg-90)*0.0174532925);
+
+    if (ss==0 || initial) {
+      initial = 0;
+      // Erase hour and minute hand positions every minute
+      tft.drawLine(ohx, ohy, 120, 121, TFT_BLACK);
+      ohx = hx*62+121;    
+      ohy = hy*62+121;
+      tft.drawLine(omx, omy, 120, 121, TFT_BLACK);
+      omx = mx*84+120;    
+      omy = my*84+121;
+    }
+
+      // Redraw new hand positions, hour and minute hands not erased here to avoid flicker
+      tft.drawLine(osx, osy, 120, 121, TFT_BLACK);
+      osx = sx*90+121;    
+      osy = sy*90+121;
+      tft.drawLine(osx, osy, 120, 121, TFT_RED);
+      tft.drawLine(ohx, ohy, 120, 121, TFT_WHITE);
+      tft.drawLine(omx, omy, 120, 121, TFT_WHITE);
+      tft.drawLine(osx, osy, 120, 121, TFT_RED);
+
+    tft.fillCircle(120, 121, 3, TFT_RED);
+  }
+}
+
+static uint8_t conv2d(const char* p) {
+  uint8_t v = 0;
+  if ('0' <= *p && *p <= '9')
+    v = *p - '0';
+  return 10 * v + *++p - '0';
+}
